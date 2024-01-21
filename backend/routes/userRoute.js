d9f502518c79c9cb68d75a63c1d0410f8024e8ef
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-21 21:35:18 +0600

    get user details route has been made and tested

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails } = require("../controllers/userController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
 
@@ -12,5 +13,7 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser ,getUserDetails);
 
 module.exports = router;
