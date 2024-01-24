3a7ec7c4a07f542775e1bad21aa87f68a9695ea8
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-24 22:44:05 +0600

    update order and delete order api has been made and tested

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -59,3 +59,73 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
       orders,
     });
   });
+
+  // get all Orders -- Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find();
+  
+    let totalAmount = 0;
+  
+    orders.forEach((order) => {
+      totalAmount += order.totalPrice;
+    });
+  
+    res.status(200).json({
+      success: true,
+      totalAmount,
+      orders,
+    });
+  });
+  
+  // update Order Status -- Admin
+  exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+  
+    if (order.orderStatus === "Delivered") {
+      return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+  
+    if (req.body.status === "Shipped") {
+      order.orderItems.forEach(async (o) => {
+        await updateStock(o.product, o.quantity);
+      });
+    }
+    order.orderStatus = req.body.status;
+  
+    if (req.body.status === "Delivered") {
+      order.deliveredAt = Date.now();
+    }
+  
+    await order.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+    });
+  });
+  
+  async function updateStock(id, quantity) {
+    const product = await Product.findById(id);
+  
+    product.Stock -= quantity;
+  
+    await product.save({ validateBeforeSave: false });
+  }
+  
+  // delete Order -- Admin
+  exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+  
+    if (!order) {
+      return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+  
+    await order.deleteOne();
+  
+    res.status(200).json({
+      success: true,
+    });
+  });
