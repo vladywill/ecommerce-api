f08d97fdf9aa6783ea557fdda5ba14d40ed4bd92
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-25 14:20:41 +0600

    roboto is used as font using webfont loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import Header from "./component/layout/Header.js";
 import {BrowserRouter as Router} from "react-router-dom";
+import webFont from "webfontloader";
+import React from 'react';
+
+
 
 function App() {
+  React.useEffect(() =>{
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  },[]);
+  
   return (
    <Router>
       <Header></Header>
