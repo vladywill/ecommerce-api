6d4912c51adb3efcc8ffd70796ca90cdfa080e31
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-28 12:01:52 +0600

    error has been imported and used

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -6,17 +6,22 @@ import MetaData from '../layout/MetaData';
 import { useSelector, useDispatch } from "react-redux";
 import { getProduct } from '../../actions/productAction.js';
 import Loader from '../layout/Loader/Loader.js';
-
+import { useAlert } from 'react-alert';
 
 const Home = () => {
+
+  const alert=useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productsCount } = useSelector(
     (state) => state.products
   );
 
   useEffect(() =>{
+    if(error){
+      return alert.error(error);
+    }
     dispatch(getProduct());
-  },[dispatch])
+  },[dispatch, error])
 
   return (
   <Fragment>
