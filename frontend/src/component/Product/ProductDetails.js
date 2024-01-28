824ce093132f96e62498d4c84dfed6477a38585d
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-28 16:16:10 +0600

    product detail page has been designed properly

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { Fragment, useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
+import { useParams } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
 
-const ProductDetails = () => {
+const ProductDetails = ({ match }) => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
+
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  }
+
   return (
-    <div>ProductDetails</div>
-  )
-}
+    <Fragment>
+      <div className="ProductDetails">
+        <div>
+          <Carousel>
+            {product.images &&
+              product.images.map((item, i) => (
+                <img
+                  className="CarouselImage"
+                  key={i}
+                  src={item.url}
+                  alt={`${i} Slide`}
+                />
+              ))}
+          </Carousel>
+        </div>
+        <div>
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+          <div className="detailsBlock-2">
+            <ReactStars {...options}></ReactStars>
+            <span>({product.numOfReviews} Reviews)</span>
+          </div>
+          <div className="detailsBlock-3">
+            <h1>{`$${product.price}`}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button>-</button>
+                <input type="number" value="1"/>
+                <button>+</button>
+              </div>
+              <button>Add to Cart</button>
+            </div>
+            <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+          </div>
+          <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+              <button className="submitReview">
+                Submit Review
+              </button>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
