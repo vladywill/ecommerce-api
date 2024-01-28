d131bad2abbac5b3a0260f7ae108957b3cb4cffb
vladywill <vladyslavmartyniuk13@gmail.com>
2024-01-29 00:16:27 +0600

    clearError has been imported in productDetail and Home file

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -2,22 +2,29 @@ import React, { Fragment, useEffect } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
-import { getProductDetails } from "../../actions/productAction";
+import { clearErrors, getProductDetails } from "../../actions/productAction";
 import { useParams } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 import ReviewCard from "./ReviewCard.js";
-
+import Loader from "../layout/Loader/Loader.js";
+import { useAlert } from "react-alert";
 
 const ProductDetails = ({ match }) => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const alert = useAlert();
+
   const { product, loading, error } = useSelector(
     (state) => state.productDetails
   );
 
   useEffect(() => {
+    if (error) {
+        alert.error(error);
+        dispatch(clearErrors());
+      } 
     dispatch(getProductDetails(id));
-  }, [dispatch, id]);
+  }, [dispatch, id, error, alert]);
 
   const options = {
     edit: false,
@@ -26,62 +33,64 @@ const ProductDetails = ({ match }) => {
     size: window.innerWidth < 600 ? 20 : 25,
     value: product.ratings,
     isHalf: true,
-  }
+  };
 
   return (
     <Fragment>
-      <div className="ProductDetails">
-        <div>
-          <Carousel>
-            {product.images &&
-              product.images.map((item, i) => (
-                <img
-                  className="CarouselImage"
-                  key={i}
-                  src={item.url}
-                  alt={`${i} Slide`}
-                />
-              ))}
-          </Carousel>
-        </div>
-        <div>
-          <div className="detailsBlock-1">
-            <h2>{product.name}</h2>
-            <p>Product # {product._id}</p>
-          </div>
-          <div className="detailsBlock-2">
-            <ReactStars {...options}></ReactStars>
-            <span>({product.numOfReviews} Reviews)</span>
-          </div>
-          <div className="detailsBlock-3">
-            <h1>{`$${product.price}`}</h1>
-            <div className="detailsBlock-3-1">
-              <div className="detailsBlock-3-1-1">
-                <button>-</button>
-                <input type="number" value="1"/>
-                <button>+</button>
-              </div>
-              <button>Add to Cart</button>
+      {loading ? (
+        <Loader></Loader>
+      ) : (
+        <Fragment>
+          <div className="ProductDetails">
+            <div>
+              <Carousel>
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
             </div>
-            <p>
+            <div>
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <ReactStars {...options}></ReactStars>
+                <span>({product.numOfReviews} Reviews)</span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`$${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button>-</button>
+                    <input type="number" value="1" />
+                    <button>+</button>
+                  </div>
+                  <button>Add to Cart</button>
+                </div>
+                <p>
                   Status:
                   <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
                     {product.Stock < 1 ? "OutOfStock" : "InStock"}
                   </b>
                 </p>
-          </div>
-          <div className="detailsBlock-4">
+              </div>
+              <div className="detailsBlock-4">
                 Description : <p>{product.description}</p>
               </div>
-              <button className="submitReview">
-                Submit Review
-              </button>
-        </div>
-      </div>
-      
-      <h3 className="reviewsHeading">REVIEWS</h3>
+              <button className="submitReview">Submit Review</button>
+            </div>
+          </div>
 
-      {product.reviews && product.reviews[0] ? (
+          <h3 className="reviewsHeading">REVIEWS</h3>
+
+          {product.reviews && product.reviews[0] ? (
             <div className="reviews">
               {product.reviews &&
                 product.reviews.map((review) => (
@@ -91,7 +100,8 @@ const ProductDetails = ({ match }) => {
           ) : (
             <p className="noReviews">No Reviews Yet</p>
           )}
-
+        </Fragment>
+      )}
     </Fragment>
   );
 };
