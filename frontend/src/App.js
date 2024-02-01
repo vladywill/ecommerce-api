a83d9263f43681fe29e3b13ce1b1d5063615a94d
vladywill <vladyslavmartyniuk13@gmail.com>
2024-02-01 12:46:45 +0600

    login and signup form has been created and styled properly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js"
 import Search from "./component/Product/Search.js"
 import { Switch } from "react-router-dom/cjs/react-router-dom.min.js";
+import LoginSignUp from "./component/User/LoginSignUp.js";
 
 
 function App() {
@@ -28,7 +29,10 @@ function App() {
         <Route exact path="/product/:id" component={ProductDetails}></Route>
         <Route exact path="/products" component={Products}></Route>
         <Route path="/products/:keyword" component={Products}></Route>
+
         <Route exact path="/search" component={Search}></Route>
+
+        <Route exact path="/login" component={LoginSignUp}></Route>
       </Switch>
 
       <Footer></Footer>
