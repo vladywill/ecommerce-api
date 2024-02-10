c4932090d20084cb3b917187ecbae14336b85a15
vladywill <vladyslavmartyniuk13@gmail.com>
2024-02-10 14:18:09 +0600

    register user functionality has been added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,18 @@ const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
+const cloudinary = require("cloudinary");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "avatars",
+        width: 150,
+        crop: "scale",
+      });
+
+
     const {name, email, password} = req.body;
 
     const user = await User.create({
@@ -14,12 +23,12 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         email,
         password,
         avatar:{
-            public_id: "this is a sample id",
-            url:"profilepicUrl",
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
         },
     });
 
-    const token = user.getJWTToken();
+    // const token = user.getJWTToken();
 
     sendToken(user, 201, res);
 });
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
-const connectDatabase = require("./config/database")
+const cloudinary = require("cloudinary");
+const connectDatabase = require("./config/database");
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -15,6 +16,12 @@ dotenv.config({path:"backend/config/config.env"});
 
 // Connecting to database
 connectDatabase();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 
 const server = app.listen(process.env.PORT,()=>{
