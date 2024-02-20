6df8a2cde71b5cea1cba31a5bee0292485565914
vladywill <vladyslavmartyniuk13@gmail.com>
2024-02-21 00:25:47 +0600

    delete product function has been made in the backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -104,19 +104,23 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 // Delete Product
-
 exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return next(new ErrorHandler("Product Not Found", 404));
+    return next(new ErrorHander("Product not found", 404));
+  }
+
+  // Deleting Images From Cloudinary
+  for (let i = 0; i < product.images.length; i++) {
+    await cloudinary.v2.uploader.destroy(product.images[i].public_id);
   }
 
   await product.deleteOne();
 
   res.status(200).json({
     success: true,
-    message: "Product Deleted Successfully",
+    message: "Product Delete Successfully",
   });
 });
 
